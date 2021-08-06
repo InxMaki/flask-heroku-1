b322d7a5298c9ac4a4e1208d38d8cd72cee5da45
InxMaki <87840557+InxMaki@users.noreply.github.com>
2021-08-07 01:38:06 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
-
 data = [
         {
             "id": 1,
@@ -26,11 +24,19 @@ return "hello Flask-Heroku"
 
 @app.route('/api', methods=['GET'])
 def get_api():
-    return jsonify(data)
 
+    
+          
+            
+    
+
+          
+    
+    
+  
+    return jsonify(data)
 @app.route('/name')
 def name():
     return "เจนจิราพร ทองแดง เลขที่ 10 ม.4/10"
-
 if __name__ == "__main__":
     app.run(debug=False)
